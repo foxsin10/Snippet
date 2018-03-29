3a9aa1ff1a1846261208e0f96f2ccdfb924b80fd
foxsin10 <fox_we10@163.com>
2018-03-29 17:47:26 +0800

    update podtrunk.swift

--- SwiftSnippet.podspec
+++ SwiftSnippet.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |s|
   #
 
   s.name         = "SwiftSnippet"
-  s.version      = "0.1.1"
+  s.version      = "0.1.3"
   s.summary      = "some code snippet with namespace."
 
   # This description is used to generate tags and improve search results.
--- podtrunk.swift
+++ podtrunk.swift
@@ -37,69 +37,52 @@ struct VersionInfo {
     }
 }
 
-protocol ExcutableCommand {
-    var path: String { get }
-    var arguments: [String] { get set }
-
-    func excute()
-}
-
-extension ExcutableCommand {
-    func excute() {
-
-        let task = Process()
-        task.launchPath = path
-        task.arguments = arguments
-
-        task.launch()
-        task.waitUntilExit()
-    }
-}
-
-
 // MARK: - git
-struct Git: ExcutableCommand {
-    private let gitpath: String = "/usr/bin/git"
-    var arguments: [String]
-    var path: String {
-        return gitpath
+struct Git {
+
+    private let tagInfo: String
+    private let gitPath: String
+
+    init(_ tagInfo: String, _ gitPath: String) {
+        self.tagInfo = tagInfo
+        self.gitPath = gitPath
     }
-    init(_ argument: [String]) {
-        self.arguments = argument
+
+    func checkTags() -> [String] {
+
+        return  excute(gitPath, ["tag"])
+            .result
+            .components(separatedBy: "")
+    }
+
+    func tag() {
+        excute(gitPath, ["tag", tagInfo])
+    }
+
+    func pushTags() {
+        excute(gitPath, ["push", "--tags"])
     }
 }
 
 // MARK: - pod
-struct Pod: ExcutableCommand {
-    private var podPath: String
-    var path: String {
-        return podPath
-    }
-    var arguments: [String]
-    init(_ podPath: String, arguments: [String]) {
+struct Pod {
+
+    private let podspec: String
+    private let podPath: String
+
+    init(_ podspec: String, _ podPath: String) {
+        self.podspec = podspec
         self.podPath = podPath
-        self.arguments = arguments
     }
-}
 
-// MARK: - shell
-struct Shell {
-    var type: String
-    var arguments: [String]
-
-    func excute()  {
-
-        var path = "/bin"
-        path += "/"
-        path += type
-
-        let task = Process()
-        task.launchPath = path
-        task.arguments = arguments
-
-        task.launch()
-        task.waitUntilExit()
+    func specLint() {
+        excute(podPath, ["spec", "lint", podspec])
     }
+
+    func podTrunk() {
+        excute(podPath,["trunk", "push", podspec])
+    }
+
 }
 
 
@@ -131,7 +114,65 @@ func validateVersion(_ version: String?) -> VersionInfo {
     return info
 }
 
-print("input version, podPath, podSpecfile:")
+@discardableResult
+func excute(_ host: String, _ arguments: [String], _ shorPath: Bool = true) -> (statusCode: Int32, result: String) {
+
+    var path = "/usr/bin/" + host
+    // 不是全路径
+    if shorPath {
+        path = host
+    }
+
+
+    let task = Process()
+    task.launchPath = path
+    task.arguments = arguments
+
+    let pipe = Pipe()
+    task.standardOutput = pipe
+
+
+    task.launch()
+    task.waitUntilExit()
+
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    var output: String = String(data: data, encoding: .utf8)!
+    output = output.replacingOccurrences(of: "\n", with: "")
+
+    return (task.terminationStatus, output)
+}
+
+@discardableResult
+func pwd() -> String {
+    return excute("/usr/bin/",["pwd"]).result
+}
+
+@discardableResult
+func findCommander(_ commander: String) -> String {
+    let host = "which"
+    return excute(host, [commander], false).result
+}
+
+@discardableResult
+func excuteShell(_ arguments: [String]) -> (Int32, String) {
+    let path = "env"
+    return excute(path, arguments, false)
+}
+
+
+//let p = excute("/usr/bin/env",["pwd"])
+
+//print(p)
+//let fileManager = FileManager.default
+//let a = try! fileManager.contentsOfDirectory(atPath: p)
+//print(a)
+
+
+
+
+
+/******************/
+print("input version, podSpecfile:")
 
 guard let inputInfo = readLine() else {
     print("invalid input info")
@@ -140,7 +181,7 @@ guard let inputInfo = readLine() else {
 
 let argus = inputInfo.components(separatedBy: " ")
 
-guard argus.count == 3 else {
+guard argus.count == 2 else {
     print("invalid input info")
     exit(0)
 }
@@ -151,23 +192,26 @@ guard info.valid == true else {
     exit(0)
 }
 
-let gitTag: Git = .init(["tag", info.version])
-gitTag.excute()
-
-let gitPushTag: Git = .init(["push", "--tags"])
-gitPushTag.excute()
 
 
-let path = argus[1]
 
-var podspec = argus[2]
+var podspec = argus[1]
 podspec += ".podspec"
+let podPath = findCommander("pod")
+let gitPath = findCommander("git")
 
-let podLint: Pod = .init(path, arguments: ["spec", "lint", podspec])
-podLint.excute()
+let git: Git = .init(info.version, gitPath)
 
-let podPush: Pod = .init(path, arguments: ["trunk", "push", podspec])
-podPush.excute()
+let pod: Pod = .init(podspec, podPath)
+
+git.tag()
+git.pushTags()
+
+pod.specLint()
+pod.podTrunk()
+
+
+
 
 
 
